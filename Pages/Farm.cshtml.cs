be91d0f14d427d2b369d0230f0ac923af385ba9a
Petr Dědic <petr.dedic@pslib.cz>
2020-04-22 16:30:22 +0200

    Initial

---
+++ Pages/Farm.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RPG1
+{
+    public class FarmModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
